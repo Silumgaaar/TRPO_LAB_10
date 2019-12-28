3e16417ab886519508ea39bfcff25eb214971329
Яркин Антон <55685055+Silumgaaar@users.noreply.github.com>
2019-12-28 23:57:04 +0300

    Проект

---
+++ LAB_10/LAB_10/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int Addition(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << Addition(a, b);
+
+}
